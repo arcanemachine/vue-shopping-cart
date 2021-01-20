39b5372711fb2979a48431289df73a71c53231b0
Nicholas Moen <arcanemachine@tutanota.com>
2021-01-20 10:13:52 -0700

    Add user + login status

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,34 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userIsAuthenticated: false,
+    user: undefined,
+  },
+  getters: {
+    userIsAuthenticated (state) {
+      return state.userIsAuthenticated;
+    },
+    user (state) {
+      return state.user;
+    }
   },
   mutations: {
+    userIsAuthenticated (state, bool) {
+      state.userIsAuthenticated = bool;
+    },
+    userIs (state, user) {
+      state.user = user;
+    }
   },
   actions: {
+    userLogin (context, user) {
+      context.commit('userIsAuthenticated', true);
+      context.commit('userIs', user);
+    },
+    userLogout (context) {
+      context.commit('userIsAuthenticated', false);
+      context.commit('userIs', undefined);
+    }
   },
   modules: {
   }
