3c3e71b586649b193b32fce604b1604b15e1093a
Nicholas Moen <arcanemachine@tutanota.com>
2021-01-20 10:12:34 -0700

    Add user icon to NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,8 +4,11 @@
       <div class="navbar-brand">
         <a class="navbar-item brand-text has-text-weight-bold" href="/">Vue Store + Shopping Cart</a>
         <div class="navbar-item-touch is-hidden-desktop is-hidden-widescreen">
-          <a class="navbar-item navbar-cart-container-touch">
-            <i class="bi-cart2 navbar-cart-touch has-text-light"></i>
+          <a class="navbar-item navbar-show-icon-touch-container" alt="User Profile Icon" title="Your Account">
+            <i class="bi-person-circle navbar-show-icon-touch has-text-light icon-person-circle"></i>
+          </a>
+          <a class="navbar-item navbar-show-icon-touch-container" alt="Shopping Cart Icon" title="Your Cart">
+            <i class="bi-cart2 navbar-show-icon-touch has-text-light"></i>
           </a>
         </div>
         <span @click="navbarToggle" class="navbar-burger burger hover-background-white" :class="navbarMenuClass" data-target="navMenu">
@@ -17,9 +20,10 @@
       <div id="navbarMenu" class="navbar-menu is-dark" :class="navbarMenuClass">
           <div class="navbar-end">
             <a class="navbar-item is-hidden-mobile is-hidden-tablet-only">
-              Your Cart <span class="navbar-cart-container-wide"><i class="bi-cart2 is-size-5 shopping-cart-icon-wide"></i></span>
+              Your Cart <span class="navbar-icon-container-wide"><i class="bi-cart2 is-size-5"></i></span>
             </a>
             <a class="navbar-item has-text-centered">Stores</a>
+            <a class="navbar-item has-text-centered">Your Account</a>
             <a class="navbar-item has-text-centered">Your Account</a>
           </div>
       </div>
@@ -68,7 +72,7 @@ export default {
 	align-items: center;
 }
 
-.navbar-cart-container-touch {
+.navbar-show-icon-touch-container {
   margin-right: 0.2rem;
 	padding-bottom: 0.25rem;
   border-radius: 0.5rem;
@@ -80,18 +84,21 @@ export default {
   transition: background-color 0.5s;
 }
 
-.hover-background-white:hover, .navbar-item.navbar-cart-container-touch:hover {
+.hover-background-white:hover, .navbar-item.navbar-icon-container-touch:hover {
   background-color: rgb(255, 255, 255, 0.2);
 }
 
-.navbar-cart-touch {
+.navbar-show-icon-touch {
   margin: -0.35rem 0 0.35rem;
 }
 
-.navbar-cart-container-wide {
+.navbar-icon-container-wide {
   margin: -0.25rem 0.25rem 0.25rem;
 }
 
+.icon-person-circle {
+  margin-top: 0;
+}
 
 </style>
 
