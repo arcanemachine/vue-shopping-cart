bec835be869de568476a29f2c12be9cd93a39016
Nicholas Moen <arcanemachine@tutanota.com>
2021-01-30 18:35:39 -0700

    Use stubbed computed properties to ensure that iconCartItemCount returns 99 if cartItemCount > 99

--- tests/unit/NavBar.spec.js
+++ tests/unit/NavBar.spec.js
@@ -1,35 +1,60 @@
 import { mount, createLocalVue } from '@vue/test-utils'
-import Vuex from 'vuex'
 import NavBar from '@/components/NavBar.vue'
 
 // local vue
-// const localVue = createLocalVue()
-const localVue = createLocalVue({store})
-localVue.use(Vuex)
-
-// vuex store
+const localVue = createLocalVue()
 
 // mocks
 import { store } from '../dummy/mocks/'
+// import store from '@/store/index.js'
 
 // helpers
 import * as helpers from '@/assets/js/helpers.js'
 localVue.prototype.$helpers = helpers;
 
 describe('NavBar.vue', () => {
-  let localStore;
+  let wrapper;
+  let defaultArgs;
 
   beforeEach(() => {
-  })
-
-  it('methods.navbarToggle() toggles data.navbarIsActive', async () => {
-    const wrapper = mount(NavBar, {
+    defaultArgs = {
       localVue,
       stubs: ['router-link', 'router-view'],
       mocks: {
         $store: store
-       }
-    })
+      }
+    }
+  })
+
+  let mountComponent = ((args={}, mocks={}, stubs=[], computed={}) => {
+    args = Object.assign({
+      localVue,
+      stubs: ['router-link', 'router-view'],
+      mocks: {
+        $store: store
+      },
+      computed: {}
+    }, args)
+    args.mocks = Object.assign(args.mocks, mocks);
+    wrapper = mount(NavBar, args)
+  });
+
+  it("computed.iconCartItemCount returns cartItemCount if item count < 99", () => {
+    mountComponent();
+    expect(wrapper.vm.iconCartItemCount).toEqual(0);
+  })
+
+  it("computed.iconCartItemCount returns 99 if item count > 99", () => {
+    mountComponent({
+      computed: {
+        cartItemCount: () => 100
+      }
+    });
+    expect(wrapper.vm.iconCartItemCount).toEqual(99);
+  })
+
+  it("methods.navbarToggle() toggles data.navbarIsActive", () => {
+    mountComponent();
 
     // navbarIsActive initially set to false
     expect(wrapper.vm.navbarIsActive).toEqual(false);
@@ -38,4 +63,5 @@ describe('NavBar.vue', () => {
     wrapper.vm.navbarToggle();
     expect(wrapper.vm.$data.navbarIsActive).toEqual(true);
   })
+
 })
