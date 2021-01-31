f1fb9aa3e759acb74fe489b191e9bfc4f5d142b8
Nicholas Moen <arcanemachine@tutanota.com>
2021-01-30 22:48:30 -0700

    Working example of using actual Vuex store in tests

--- tests/unit/NavBar.spec.js
+++ tests/unit/NavBar.spec.js
@@ -2,11 +2,13 @@ import { mount, createLocalVue } from '@vue/test-utils'
 import NavBar from '@/components/NavBar.vue'
 
 // local vue
+import Vuex from 'vuex'
 const localVue = createLocalVue()
+localVue.use(Vuex)
 
 // mocks
-import { store } from '../dummy/mocks/'
-// import store from '@/store/index.js'
+// import { store } from '../dummy/mocks/'
+import store from '@/store/index.js'
 
 // helpers
 import * as helpers from '@/assets/js/helpers.js'
@@ -14,45 +16,71 @@ localVue.prototype.$helpers = helpers;
 
 describe('NavBar.vue', () => {
   let wrapper;
-  let defaultArgs;
+  let defaultParams;
 
   beforeEach(() => {
-    defaultArgs = {
+    defaultParams = {
       localVue,
+      store,
       stubs: ['router-link', 'router-view'],
       mocks: {
-        $store: store
+        // $store: store
       }
     }
   })
 
-  let mountComponent = ((args={}, mocks={}, stubs=[], computed={}) => {
-    args = Object.assign({
-      localVue,
-      stubs: ['router-link', 'router-view'],
-      mocks: {
-        $store: store
-      },
-      computed: {}
-    }, args)
-    args.mocks = Object.assign(args.mocks, mocks);
-    wrapper = mount(NavBar, args)
+  let mountComponent = ((params={}, data={}, mocks={}, stubs=[], computed={}) => {
+    let componentArgs = Object.assign(defaultParams, params)
+    Object.assign(componentArgs.mocks, mocks);
+    wrapper = mount(NavBar, componentArgs)
   });
 
-  it("computed.iconCartItemCount returns cartItemCount if item count < 99", () => {
+  // computed.iconCartItemCount
+  it("computed.iconCartItemCount returns cartItemCount if item count <= 99", () => {
     mountComponent();
     expect(wrapper.vm.iconCartItemCount).toEqual(0);
   })
 
   it("computed.iconCartItemCount returns 99 if item count > 99", () => {
     mountComponent({
-      computed: {
-        cartItemCount: () => 100
-      }
+      computed: {cartItemCount: () => 100}
     });
     expect(wrapper.vm.iconCartItemCount).toEqual(99);
   })
 
+  // computed.navbarMenuClass
+  it("computed.navbarMenuClass returns false if !data.navbarIsActive", () => {
+    mountComponent({
+      data: () => {
+        return {navbarIsActive: false}
+      }
+    })
+    expect(wrapper.vm.navbarMenuClass).toEqual({'is-active': false});
+  })
+
+  it("computed.navbarMenuClass returns true if data.navbarIsActive", () => {
+    mountComponent({
+      data: () => {
+        return {navbarIsActive: true}
+      }
+    });
+    expect(wrapper.vm.navbarMenuClass).toEqual({'is-active': true});
+  })
+
+  // computed.userIconUrl
+  it("computed.userIconUrl returns {name: 'login'} if !store.state.userIsAuthenticated", () => {
+    
+    mountComponent({
+    })
+    expect(wrapper.vm.userIconUrl).toEqual({name: 'login'});
+  })
+
+  it("computed.userIconUrl returns {name: 'login'} if !store.state.userIsAuthenticated", () => {
+    mountComponent({})
+    expect(wrapper.vm.userIconUrl).toEqual({name: 'login'});
+  })
+
+  // methods.navbarMenuClass
   it("methods.navbarToggle() toggles data.navbarIsActive", () => {
     mountComponent();
 
