970a8014e0c5a2d3e9fbe30c794661e43810eebb
Nicholas Moen <arcanemachine@tutanota.com>
2021-02-01 10:10:12 -0700

    Create test to detect presence of introSeen cookie

--- tests/e2e/testcafe/test.js
+++ tests/e2e/testcafe/test.js
@@ -1,4 +1,4 @@
-import { Selector, RequestLogger } from 'testcafe';
+import { Selector, RequestLogger, ClientFunction } from 'testcafe';
 import * as helpers from '../../../src/assets/js/helpers.js'
 
 // fixture `Getting Started`
@@ -14,24 +14,60 @@ import * as helpers from '../../../src/assets/js/helpers.js'
 // 
 // });
 
-const url = `${helpers.BACKEND_SERVER_URL}/`
+const domainUrl = `${helpers.FRONTEND_SERVER_URL}`
 
-const logger = RequestLogger({url}, {
+
+const setCookie = ClientFunction((key, value) => {
+	document.cookie = `${key}=${value}`;
+})
+
+const getCookie = ClientFunction((key) => {
+	try {
+		document.cookie.split('; ').map(x => x.split('=')).filter(x => x[0] === key)[0][1];
+	}
+	catch(err) {
+		return false;
+	}
+	return document.cookie;
+})
+
+const getCookies = ClientFunction(() => {
+	return document.cookie;
+})
+
+
+// intro
+const logger = RequestLogger({domainUrl}, {
 	logResponseHeaders: true
 });
 
-fixture `Hello World`
-	.page(url)
+fixture `Intro`
+	.page(`${domainUrl}/`)
 	.requestHooks(logger);
 
 test('Page returns 200', async t => {
 	await t.expect(logger.contains(r => r.response.statusCode === 200)).ok();
 })
 
+test('Browser sets introSeen cookie on page load', async t => {
+	await t.expect(getCookie('introSeen')).ok();;
+})
 
+
+// login
 fixture `views.Login`
+	.page(`${domainUrl}/login/`)
 
 test('User can login', async t => {
 	const username = 'test_user';
 	const password = 'drowssap';
+
+	const usernameField = Selector('#login-input-username');
+	const passwordField = Selector('#login-input-password');
+
+	// username field is autofocused
+	// await t.expect(usernameField.value).eql('', 'input is empty');
+
+	// submit username and password
+
 })
